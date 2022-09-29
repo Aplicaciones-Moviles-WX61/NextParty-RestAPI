07512e68f231d3c010820df17f94e4780017b05f
John Leon <leonsahir7@gmail.com>
2022-09-28 22:40:12 -0500

    add database configuration

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,22 +2,22 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
 import { ItemsModule } from './items/items.module';
 import { PartyModule } from './parties/party.module';
 import { TypeModule } from './types_of/types.module';
 import { UserModule } from './users/user.module';
 import { WishlistModule } from './wishlists/whislist.module';
-import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
     TypeOrmModule.forRoot({
       type: 'mysql',
-      host: 'localhost',
+      host: 'nextparty.mysql.database.azure.com',
       port: 3306,
-      username: 'root',
-      password: '123456',
-      database: 'test',
+      username: 'admin1',
+      password: 'NextParty1',
+      database: 'v1',
       autoLoadEntities: true,
     }),
     UserModule,
