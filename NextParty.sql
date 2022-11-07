d1588663703df5af41c0c78542c3892bc8717683
John Leon <leonsahir7@gmail.com>
2022-11-07 09:37:21 -0500

    change database generator

--- NextParty.sql
+++ NextParty.sql
@@ -2,7 +2,7 @@ CREATE TABLE `categories` (
   `id` int NOT NULL AUTO_INCREMENT,
   `category` varchar(20) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+);
 
 CREATE TABLE `items` (
   `id` int NOT NULL AUTO_INCREMENT,
@@ -13,7 +13,7 @@ CREATE TABLE `items` (
   `image` varchar(100) DEFAULT NULL,
   `category_id` int NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+);
 
 CREATE TABLE `parties` (
   `id` int NOT NULL AUTO_INCREMENT,
@@ -23,27 +23,27 @@ CREATE TABLE `parties` (
   `date` datetime DEFAULT NULL,
   `image` varchar(100) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+);
 
 CREATE TABLE `roles` (
   `id` int NOT NULL AUTO_INCREMENT,
   `role` varchar(20) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+);
 
 CREATE TABLE `user_item` (
   `user_id` int NOT NULL,
   `item_id` int NOT NULL,
   `obtained_on` datetime DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`user_id`,`item_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+);
 
 CREATE TABLE `user_party` (
   `user_id` int NOT NULL,
   `party_id` int NOT NULL,
   `role_Id` int NOT NULL DEFAULT '1',
   PRIMARY KEY (`user_id`,`party_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+);
 
 CREATE TABLE `users` (
   `id` int NOT NULL AUTO_INCREMENT,
@@ -54,7 +54,7 @@ CREATE TABLE `users` (
   `phone` varchar(9) DEFAULT NULL,
   `birthday` date DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+);
 
 CREATE TABLE `wishlists` (
   `id` int NOT NULL AUTO_INCREMENT,
@@ -62,14 +62,14 @@ CREATE TABLE `wishlists` (
   `description` varchar(200) NOT NULL,
   `image` varchar(200) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+);
 
 Alter table items add CONSTRAINT `Items_Category` FOREIGN KEY (`category_id`) REFERENCES `categories` (`id`);
 Alter table items add CONSTRAINT `Items_Wishlists` FOREIGN KEY (`party_id`) REFERENCES `wishlists` (`id`);
-Alter table user_item add CONSTRAINT `User_Item_Items` FOREIGN KEY (`item_id`) REFERENCES `items` (`id`);
-Alter table user_item add CONSTRAINT `User_Item_Users` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`);
-Alter table user_party add CONSTRAINT `user_party_Parties` FOREIGN KEY (`party_id`) REFERENCES `parties` (`id`);
-Alter table user_party add CONSTRAINT `user_party_Users` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`);
-ALTER TABLE user_party ADD CONSTRAINT `user_party_role` FOREIGN KEY User_Party_Role (`role_id`) REFERENCES Roles (id);
-Alter table wishlists add CONSTRAINT `Wishlists_Parties` FOREIGN KEY (`party_id`) REFERENCES `parties` (`id`) ;
+Alter table user_item add CONSTRAINT `User_Items` FOREIGN KEY (`item_id`) REFERENCES `items` (`id`);
+Alter table user_item add CONSTRAINT `Users_Item` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`);
+Alter table user_party add CONSTRAINT `user_parties` FOREIGN KEY (`party_id`) REFERENCES `parties` (`id`);
+Alter table user_party add CONSTRAINT `party_users` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`);
+ALTER TABLE user_party add CONSTRAINT `user_party_role` FOREIGN KEY (`role_id`) REFERENCES  `roles` (`id`);
+Alter table wishlists add CONSTRAINT `wishlists_parties` FOREIGN KEY (`party_id`) REFERENCES `parties` (`id`) ;
 
